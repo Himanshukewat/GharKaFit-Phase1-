ebb499cb4a5a114a2ecd8bf483002e65e0300b45
Himanshu Kewat <himanshukewat2810@gmail.com>
2026-03-01 15:22:20 +0530

    add goalSelection component and screen, vm, vmf , enum class

---
+++ app/src/main/java/com/example/gharkafit/model/Goal.kt
@@ -0,0 +1,7 @@
+package com.example.gharkafit.model
+
+enum class Goal {
+    FAT_LOSS,
+    MUSCLE_GAIN,
+    MAINTAIN
+}
---
+++ app/src/main/java/com/example/gharkafit/ui/screen/goalSelection/GoalSelectionVMF.kt
@@ -0,0 +1,20 @@
+package com.example.gharkafit.ui.screen.goalSelection
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.gharkafit.data.user.UserDao
+
+class GoalSelectionVMF(
+    private val userDao: UserDao
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        if (modelClass.isAssignableFrom(GoalSelectionVM::class.java)) {
+
+            return GoalSelectionVM(userDao) as T
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
