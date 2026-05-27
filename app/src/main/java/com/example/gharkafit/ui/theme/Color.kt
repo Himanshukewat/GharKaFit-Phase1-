8f3844954901e2840b5d099aac3d570b1d5ce554
Himanshu Kewat <himanshukewat2810@gmail.com>
2026-05-27 22:21:58 +0530

    add green color in default in color and theme file

--- app/src/main/java/com/example/gharkafit/ui/theme/Color.kt
+++ app/src/main/java/com/example/gharkafit/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryGreen80 = Color(0xFFA5D6A7)
+val SecondaryGreen80 = Color(0xFFC8E6C9)
+val AccentGreen80 = Color(0xFFE8F5E9)
+
+val PrimaryGreen40 = Color(0xFF4CAF50)
+val SecondaryGreen40 = Color(0xFF66BB6A)
+val AccentGreen40 = Color(0xFF81C784)
+
+val PrimaryGreen = Color(0xFF4CAF50)
--- app/src/main/java/com/example/gharkafit/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gharkafit/ui/theme/Theme.kt
@@ -12,25 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryGreen80,
+    secondary = SecondaryGreen80,
+    tertiary = AccentGreen80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryGreen40,
+    secondary = SecondaryGreen40,
+    tertiary = AccentGreen40
 )
 
 @Composable
