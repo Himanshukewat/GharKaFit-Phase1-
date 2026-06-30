e82a4b4f201ed8c9450e7a29641d6af72a65aef3
Himanshu Kewat <himanshukewat2810@gmail.com>
2026-06-30 14:25:22 +0530

    feat: Calculator are fix and make userEntity independent

--- app/src/main/java/com/example/gharkafit/core/Calculator.kt
+++ app/src/main/java/com/example/gharkafit/core/Calculator.kt
@@ -1,50 +1,69 @@
 package com.example.gharkafit.core
 
-import com.example.gharkafit.data.user.UserEntity
-
 object Calculator {
-    fun calculateBMI(weightKg: Double, heightCm: Double): Double {
+
+    fun calculateBMI(
+        weightKg: Double,
+        heightCm: Double
+    ): Double {
+
         val heightM = heightCm / 100
         return weightKg / (heightM * heightM)
     }
-    fun calculateDailyCalories(user: UserEntity): Int {
-        // BMR (gender based)
-        val bmr = if (user.gender == "MALE") {
-            10 * user.weightKg +
-                    6.25 * user.heightCm -
-                    5 * user.age + 5
+
+    fun calculateDailyCalories(
+        weightKg: Double,
+        heightCm: Double,
+        age: Int,
+        gender: String,
+        activityLevel: String,
+        goal: String
+    ): Int {
+
+        // BMR
+        val bmr = if (gender == "MALE") {
+            10 * weightKg +
+                    6.25 * heightCm -
+                    5 * age + 5
         } else {
-            10 * user.weightKg +
-                    6.25 * user.heightCm -
-                    5 * user.age - 161
+            10 * weightKg +
+                    6.25 * heightCm -
+                    5 * age - 161
         }
 
-        // Activity multiplier
-        val activityMultiplier = when (user.activityLevel) {
+        // Activity Multiplier
+        val activityMultiplier = when (activityLevel) {
             "SEDENTARY" -> 1.2
             "MODERATE" -> 1.55
             "ACTIVE" -> 1.75
             else -> 1.2
         }
+
         val maintenanceCalories = bmr * activityMultiplier
-        //Goal adjustment
-        val finalCalories = when (user.goal) {
-            "FAT_LOSS" -> maintenanceCalories - 400   // deficit
-            "MUSCLE_GAIN" -> maintenanceCalories + 250 // surplus
+
+        // Goal Adjustment
+        val finalCalories = when (goal) {
+            "FAT_LOSS" -> maintenanceCalories - 400
+            "MUSCLE_GAIN" -> maintenanceCalories + 250
             "MAINTAIN" -> maintenanceCalories
             else -> maintenanceCalories
         }
+
         return finalCalories.toInt()
     }
 
-    fun calculateDailyProtein(user: UserEntity): Double {
-        val weight = user.weightKg
-        val multiplier = when(user.goal){
+    fun calculateDailyProtein(
+        weightKg: Double,
+        goal: String
+    ): Double {
+
+        val multiplier = when (goal) {
             "FAT_LOSS" -> 1.6
             "MUSCLE_GAIN" -> 1.8
             "MAINTAIN" -> 1.3
             else -> 1.3
         }
-        return weight * multiplier
+
+        return weightKg * multiplier
     }
 }
